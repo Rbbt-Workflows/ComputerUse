0e6abd264b956c8e29a31f4933340074c8739a41
Miguel Vazquez <mikisvaz@gmail.com>
2025-11-05 13:27:24 +0100

    Add python

--- lib/ComputerUse/tasks/exec.rb
+++ lib/ComputerUse/tasks/exec.rb
@@ -22,5 +22,55 @@ and STDERR outputs as strings, and exit_status, the exit status of the process
     cmd_json cmd
   end
 
+  desc <<-EOF
+Run a file or code using python.
+
+If `file` is provided it will be executed. Otherwise `code` will be written to a temporary
+file under the task `root` and executed. Returns a JSON object with keys stdout,
+stderr and exit_status.
+  EOF
+  input :code, :text, 'Python code to run (ignored if file provided)'
+  input :file, :path, 'File to run'
+  extension :json
+  task :python => :text do |code, file|
+    # Prefer provided file, otherwise write code to a temp file in root
+    if file && !file.to_s.empty?
+      root_holds_file file
+      target = file
+    elsif code && !code.to_s.empty?
+      tmp = File.join(ComputerUse.root, "agent_script_#{Time.now.to_i}_#{rand(1000)}.py")
+      File.open(tmp, 'w') { |f| f.write(code) }
+      target = tmp
+    else
+      raise ParameterException, 'Provide either a file or code to run'
+    end
+
+    # Prefer python3 if available
+    cmd_name = nil
+    ['python3', 'python'].each do |p|
+      begin
+        io_test = CMD.cmd(p.to_sym, '--version', save_stderr: true, pipe: true, no_fail: true)
+        io_test.join
+        if io_test.exit_status == 0 || io_test.read.to_s.length > 0
+          cmd_name = p
+          break
+        end
+      rescue
+        next
+      end
+    end
+    cmd_name ||= 'python'
+
+    begin
+      io = CMD.cmd(cmd_name.to_sym, target, save_stderr: true, pipe: true)
+      text = io.read
+      io.join
+      {stdout: text, stderr: io.std_err, exit_status: io.exit_status}
+    rescue
+      raise ScoutException, io.std_err
+    end
+  end
+
   export_exec :bash
+  export_exec :python
 end
